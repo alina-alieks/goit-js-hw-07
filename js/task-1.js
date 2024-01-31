fe24fe1da729476680c9a8cc982c674227f5148f
alina-alieks <145293161+alina-alieks@users.noreply.github.com>
2024-01-31 17:18:24 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -3,11 +3,19 @@ const totalCategories = categories.children.length;
 console.log("Number of categories: ", totalCategories);
 
 
-const allCategories = document.querySelectorAll(".item h2");
+// const allCategories = document.querySelectorAll(".item h2");
 
-allCategories.forEach(category => {
-    console.log(`Category: ${category.textContent}`);
-    console.log(`Elements: ${category.nextElementSibling.children.length}`)
-})
+// allCategories.forEach(category => {
+//     console.log(`Category: ${category.textContent}`);
+//     console.log(`Elements: ${category.nextElementSibling.children.length}`);
+// });
 
 
+const allCategories = document.querySelectorAll(".item");
+
+for(const category of allCategories) {
+    const nameCategory = category.querySelector("h2");
+    const itemInCategory = category.querySelector("ul");
+    console.log(`Category: ${nameCategory.textContent}`);
+    console.log(`Elements: ${itemInCategory.children.length}`);
+};
